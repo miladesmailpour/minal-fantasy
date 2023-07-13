0258a4c36aa3ba4a70c91372364f636634991c60
miladesmailpour <milad.esmailpour@gmail.com>
2023-07-13 12:14:48 -0400

    add client side query to typedefs to find a user, users and create one

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,5 +1,29 @@
-// const { gql } = require('apollo-server-express');
+const { gql } = require('apollo-server');
 
-// const typeDefs = gql``;
+const typeDefs = gql`
+  type User {
+    id: ID!
+    firstName: String!
+    lastName: String!
+    userName: String!
+    email: String!
+  }
 
-// module.exports = typeDefs;
+  type Query {
+    getUser(id: ID!): User
+    Users: [User]
+  }
+
+  type Mutation {
+    createUser(input: CreateUserInput!): User
+  }
+
+  input CreateUserInput {
+    firstName: String!
+    lastName: String!
+    userName: String!
+    email: String!
+  }
+`;
+
+module.exports = typeDefs;
