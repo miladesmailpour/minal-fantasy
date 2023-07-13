7871008e5c20fabb889dd17efe04c65c1849489b
miladesmailpour <milad.esmailpour@gmail.com>
2023-07-13 12:48:54 -0400

    getUser by email and username added

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -15,15 +15,31 @@ const resolvers = {
         const user = await User.findById(id);
         return user;
       } catch (e) {
-        throw new Error("Faild to fetch user");
+        throw new Error("Failed to fetch user");
+      }
+    },
+    getUserByEmail: async (_, { email }) => {
+      try {
+        const user = await User.findOne({ email });
+        return user;
+      } catch (e) {
+        throw new Error("Failed to fetch user");
+      }
+    },
+    getUserByUserName: async (_, { userName }) => {
+      try {
+        const user = await User.findOne({ userName });
+        return user;
+      } catch (e) {
+        throw new Error("Failed to fetch user");
       }
     },
   },
   Mutation: {
-    createUser: async (_, input) => {
+    createUser: async (_, { input }) => {
       try {
         const user = await User.create(input);
-        return users;
+        return user;
       } catch (e) {
         throw new Error("Failed to create user");
       }
