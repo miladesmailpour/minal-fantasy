ab4a47f225219d036fd4668722bd6812dd2719e4
miladesmailpour <milad.esmailpour@gmail.com>
2023-07-18 04:30:38 -0400

    gif added to playground

--- client/src/components/Tile.jsx
+++ client/src/components/Tile.jsx
@@ -22,8 +22,16 @@ const Tile = ({ value, isRevealed, onClick }) => {
 
   const clicked = state.revealed;
   let cell = '';
-  if (state.threat) cell = 'T';
-  else if (state.reward) cell = 'R';
+  const threatsGifPath = [<img src='../../assets/sprites/demon/Demon-Sprite64.gif' alt="Demon-Sprite.gif" />]
+  const rewardsGifPath = [
+    <img src='../../assets/sprites/knight/Knight-Sprite64.gif' alt="Knight-Sprite64.gif" />,
+    <img src='../../assets/sprites/undeadTree/Undead-Tree-Sprite64.gif' alt="Undead-Tree-Sprite64.gif" />,
+    <img src='../../assets/sprites/wizard/Wizard-Sprite64.gif' alt="Wizard-Sprite64.gif" />
+  ]
+  if (state.threat) {
+    cell = threatsGifPath[0];
+  }
+  else if (state.reward) cell = rewardsGifPath[Math.floor(Math.random() * 3)];
   else cell = state.adjacentThreat;
 
   return (
